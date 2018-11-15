4ff950e47a5e0b53904ac27a1708581604723c1b
Kazuito <kazit.kazuito@gmail.com>
2018-11-15 12:02:50 +0900

    Fix environment $HOME to avoid npm permission error

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -3,17 +3,9 @@
 USER_ID=`id -u`
 GROUP_ID=`id -g`
 
-# グループを作成する
-if [ "$GROUP_ID" != "0" ]; then
-    addgroup -g $GROUP_ID $USER_NAME
-fi
-
 # ユーザを作成する
 if [ "$USER_ID" != "0" ]; then
-    adduser -u $USER_ID -g $GROUP_ID $USER_NAME
+    export HOME=/tmp
 fi
-
-# パーミッションを元に戻す
-sudo chmod -s /usr/sbin/adduser /usr/sbin/addgroup
 
 exec $@
